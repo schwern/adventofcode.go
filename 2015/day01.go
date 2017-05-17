92c6fef47e2ab1ff009cc770ce2e4e511d72b0ad
Michael G. Schwern <schwern@pobox.com>
2017-05-16 17:26:23 -0700

    Implement day 1 part 1

--- 2015/day01.go
+++ 2015/day01.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
 func FindFloor(instructions string) (floor int) {
     floor = 0
+    
+    for _, char := range instructions {
+        switch char {
+            case '(':
+                floor++
+            case ')':
+                floor--
+            default:
+                fmt.Printf("Unknown character '%v'\n", char)
+        }
+    }
     
     return
 }
