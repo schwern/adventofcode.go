39f5660705b0f1aa902f93f1261fb86a1435c1e9
Michael G. Schwern <schwern@pobox.com>
2017-05-19 17:38:50 -0700

    Extract method

--- day03/day03.go
+++ day03/day03.go
@@ -2,6 +2,21 @@ package day03
 
 import "fmt"
 
+func move( direction rune ) (int, int) {
+    switch direction {
+        case '^':
+            return 0, 1
+        case 'v':
+            return 0, -1
+        case '<':
+            return -1, 0
+        case '>':
+            return 1, 0
+        default:
+            panic(fmt.Sprintf("Unknown direction %v", direction))
+    }
+}
+
 func PresentsDelivered( directions string ) int {
     count := 0
     grid := make( map[[2]int]int )
@@ -13,18 +28,9 @@ func PresentsDelivered( directions string ) int {
     count++
     
     for _, dir := range directions {        
-        switch dir {
-            case '^':
-                y++
-            case 'v':
-                y--
-            case '<':
-                x--
-            case '>':
-                x++
-            default:
-                panic(fmt.Sprintf("Unknown direction %v", dir))
-        }
+        dx, dy := move(dir)
+        x += dx
+        y += dy
         
         pos = [2]int{x,y}
         if grid[pos] == 0 {
