5105fe31d4871a84e3cf9a53c9027c34c5afaacc
Michael G. Schwern <schwern@pobox.com>
2017-05-24 12:25:48 -0700

    Day 8 Part 2

--- 2015/day08/day08.go
+++ 2015/day08/day08.go
@@ -1,6 +1,9 @@
 package day08
 
-import "unicode"
+import(
+    "unicode"
+    "github.com/schwern/adventofcode.go/util"
+)
 
 func EncodingSize( str string ) int {
     // Start with the surrounding quotes
@@ -12,6 +15,37 @@ func EncodingSize( str string ) int {
         } else {
             size += 2
         }
+    }
+    
+    return size
+}
+
+func MemorySize( str string ) int {
+    size := 0
+    
+    for i := 0; i < len(str); i++ {
+        size++
+        
+        switch str[i] {
+            case '\\':
+                switch c := str[i+1]; c {
+                    // \\ or \"
+                    case '\\', '"':
+                        i++
+                    // \x##
+                    case 'x':
+                        i+=3
+                    default:
+                        util.Panicf("Unknown escape: %v", c)
+                }
+        }
+    }
+    
+    if str[0] == '"' {
+        size--
+    }
+    if str[len(str)-1] == '"' {
+        size--
     }
     
     return size
--- 2015/day08/day08_test.go
+++ 2015/day08/day08_test.go
@@ -9,6 +9,33 @@ import(
 
 var input_file = "testdata/input.txt"
 
+func TestMemorySize( t *testing.T ) {
+    tests := []testutil.TestCase{
+        { `""`, 0 },
+        { `"abc"`, 3 },
+        { `"aaa\"aaa"`, 7 },
+        { `"\x27"`, 1 },
+    }
+    
+    for _, test := range tests {
+        testutil.AssertEq( t, day08.MemorySize( test.Arg ), test.Want )
+    }
+}
+
+func TestPart1( t *testing.T ) {
+    lines := util.LineChannel( input_file )
+    
+    size := 0
+    mem_size := 0
+    
+    for line := range lines {
+        size += len(line)
+        mem_size += day08.MemorySize( line )
+    }
+    
+    testutil.AssertEq( t, size - mem_size, 1350 )
+}
+
 func TestEncodingSize( t *testing.T ) {
     tests := []testutil.TestCase{
         { `""`, 6 },
