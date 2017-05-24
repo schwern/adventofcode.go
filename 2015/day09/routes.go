edba7d77b5ff2c3223e775bd04ed352ef84cc7fe
Michael G. Schwern <schwern@pobox.com>
2017-05-24 15:33:09 -0700

    day9: Restructure how Routes are implemented.
    Use a traditional 2D array so its easier to implement travelling salesman.
    
    Routes just holds the graph. Something else will traverse it.

--- 2015/day09/routes.go
+++ 2015/day09/routes.go
@@ -4,29 +4,73 @@ import(
     "github.com/schwern/adventofcode.go/util"
 )
 
-type RouteKey [2]string
-type Routes map[RouteKey]int
-
-func (self Routes) SetRoute( a, b string, dist int ) {
-    self[ makeKey( a, b ) ] = dist
+type Routes struct {
+    edges [][]int
+    names map[string]int
+    is_symetric bool
 }
 
-func (self Routes) GetRoute( a, b string ) int {
-    return self[ makeKey( a, b ) ]
-}
-
-func NewRoutes() Routes {
-    return make( Routes )
-}
-
-func makeKey( a,b string ) RouteKey {
-    switch {
-        case a < b:
-            return [2]string{ a, b }
-        case b < a:
-            return [2]string{ b, a }
-        default: 
-            util.Panicf("keys are equal: %v", a)
-            return [2]string{}
+func NewRoutes( symetric bool ) Routes {
+    self := Routes{
+        edges: make( [][]int, 0 ),
+        names: make( map[string]int ),
     }
+    
+    self.is_symetric = symetric
+    
+    return self
+}
+
+func (self *Routes) addNode( name string ) int {
+    len := len(self.edges)
+    
+    // Extend each existing row by 1    
+    for i := range self.edges {
+        self.edges[i] = append(self.edges[i], 0)
+    }
+    
+    // Add a new row
+    self.edges = append(self.edges, make( []int, len+1) )
+    
+    self.names[name] = len
+    
+    return len
+}
+
+func (self *Routes) getNodeIdx( name string ) (int, bool) {
+    idx, exists := self.names[name]
+    return idx, exists
+}
+
+func (self *Routes) mustGetNodeIdx( name string ) (int) {
+    idx, exists := self.getNodeIdx(name)
+    if !exists {
+        util.Panicf("Do not have a node named %v", name)
+        return 0
+    }
+    
+    return idx
+}
+
+func (self *Routes) getOrAddNodeIdx( name string ) int {
+    idx, exists := self.getNodeIdx( name )
+    if !exists {
+        idx = self.addNode( name )
+    }
+
+    return idx
+}
+
+func (self *Routes) AddRoute( a, b string, dist int ) {
+    aidx := self.getOrAddNodeIdx( a )
+    bidx := self.getOrAddNodeIdx( b )
+    
+    self.edges[ aidx ][ bidx ] = dist
+    if self.is_symetric {
+        self.edges[ bidx ][ aidx ] = dist
+    }
+}
+
+func (self *Routes) GetRoute( a, b string ) int {
+    return self.edges[ self.mustGetNodeIdx(a) ][ self.mustGetNodeIdx(b) ]
 }
--- 2015/day09/routes_test.go
+++ 2015/day09/routes_test.go
@@ -6,25 +6,24 @@ import(
     "github.com/schwern/adventofcode.go/testutil"
 )
 
-func TestShortestRoute( t *testing.T ) {
-    routes := day09.NewRoutes()
-    
-    routes.SetRoute( "London", "Dublin", 464 )
-    routes.SetRoute( "London", "Belfast", 518 )
-    routes.SetRoute( "Dublin", "Belfast", 141 )
-    
-    t.Skip("ShortestRoute not defined")
-    //testutil.AssertEq( t, routes.ShortestRoute(), 605 )
-}
-
-func TestSetGetRoute( t *testing.T ) {
-    routes := day09.NewRoutes()
-    
-    routes.SetRoute( "London", "Dublin", 464 )
-    routes.SetRoute( "London", "Belfast", 518 )
+func TestAddGetRoute( t *testing.T ) {
+    routes := day09.NewRoutes( true )
+        
+    routes.AddRoute( "London", "Dublin", 464 )
+    routes.AddRoute( "London", "Belfast", 518 )
+    routes.AddRoute( "Dublin", "Belfast", 141 )
 
     testutil.AssertEq( t, routes.GetRoute( "London", "Dublin" ), 464 )
     testutil.AssertEq( t, routes.GetRoute( "Dublin", "London" ), 464 )
     testutil.AssertEq( t, routes.GetRoute( "Belfast", "London" ), 518 )
     testutil.AssertEq( t, routes.GetRoute( "London", "Belfast" ), 518 )
+    testutil.AssertEq( t, routes.GetRoute( "Dublin", "Belfast" ), 141 )
+    testutil.AssertEq( t, routes.GetRoute( "Belfast", "Dublin" ), 141 )
+}
+
+func TestGetRouteUnknownRoute( t *testing.T ) {
+    routes := day09.NewRoutes( false )
+
+    defer testutil.AssertPanicf( t, "Do not have a node named Foo" )
+    routes.GetRoute( "Foo", "Bar" )
 }
