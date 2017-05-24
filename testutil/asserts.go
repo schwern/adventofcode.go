449337ec27c8c6ad3e71babc8ed0711923a83fc3
Michael G. Schwern <schwern@pobox.com>
2017-05-23 17:04:09 -0700

    Add an assert for comparing string slices.

--- testutil/asserts.go
+++ testutil/asserts.go
@@ -15,3 +15,20 @@ func AssertEq(t *testing.T, have, want interface{}) {
         )
     }
 }
+
+func AssertStringSliceEq( t *testing.T, have, want []string ) {
+    if len(have) != len(want) {
+        t.Errorf( "\nlen(have): %v\nlen(want): %v", len(have), len(want) )
+        return
+    }
+    
+    for i := range have {
+        if have[i] != want[i] {
+            t.Errorf(
+                "\nhave[%v]: %#v\nwant[%v]: %#v",
+                i, have[i], i, want[i],
+            )
+            return
+        }
+    }
+}
