6d254776f3dd1ed71baae4027453f4020d457444
Michael G. Schwern <schwern@pobox.com>
2017-05-27 16:36:18 -0700

    Day 12, Part 1 complete.

--- 2015/day12/day12.go
+++ 2015/day12/day12.go
@@ -1,5 +1,55 @@
 package day12
 
-func SumNums( in string ) int {
-    return 0
+import(
+    "encoding/json"
+    "sync"
+    "github.com/schwern/adventofcode.go/util"
+)
+
+func SumNums( in string ) float64 {
+    var data interface{}
+    err := json.Unmarshal( []byte(in), &data )
+    util.Check(err)
+    
+    out := make( chan float64 )    
+    go func() {
+        var wg sync.WaitGroup
+        defer close(out)
+        defer wg.Wait()
+        
+        wg.Add(1)
+        go readNums( data, out, &wg )
+    }()
+    
+    total := 0.0
+    for val := range out {
+        total += val
+    }
+    
+    return total
+}
+
+func readNums( data interface{}, out chan float64, wg *sync.WaitGroup ) {
+    defer wg.Done()
+    
+    _ = "breakpoint"
+    
+    switch d := data.(type) {
+        case int64, float64:
+            out <- data.(float64)
+        case string:
+            // ignore it
+        case []interface{}:
+            for _,u := range d {
+                wg.Add(1)
+                go readNums( u, out, wg )
+            }
+        case map[string]interface{}:
+            for _,u := range d {
+                wg.Add(1)
+                go readNums( u, out, wg )
+            }
+        default:
+            util.Panicf( "%v %T is of a type I don't know how to handle", data, data )
+    }
 }
--- 2015/day12/day12_test.go
+++ 2015/day12/day12_test.go
@@ -3,11 +3,14 @@ package day12_test
 import(
     "testing"
     "github.com/schwern/adventofcode.go/testutil"
+    "github.com/schwern/adventofcode.go/util"
     "github.com/schwern/adventofcode.go/2015/day12"
 )
 
+var Input_File = "testdata/input.txt"
+
 func TestSumNums( t *testing.T ) {
-    tests := []testutil.TestCase{
+    tests := []struct{ Arg string; Want float64 }{
         { "[1,2,3]", 6 },
         { `{"a":2,"b":4}`, 6 },
         { `[[[3]]]`, 3 },
@@ -22,4 +25,9 @@ func TestSumNums( t *testing.T ) {
         have := day12.SumNums( test.Arg )
         testutil.AssertEq( t, have, test.Want )
     }
+}
+
+func TestPart1( t *testing.T ) {
+    have := day12.SumNums( util.ReadFile( Input_File ) )
+    testutil.AssertEq( t, have, float64(119433) )
 }
