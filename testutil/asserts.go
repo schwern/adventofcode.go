05be83a713d99f7beaa9dd79f6f0bf8ae2f488c5
Michael G. Schwern <schwern@pobox.com>
2017-06-01 16:24:36 +0200

    Make AssertEq() work with just about anything.
    The downside is it doesn't say *why* they're not equal.
    That's why Assert*SliceEq() have been left alone.

--- testutil/asserts.go
+++ testutil/asserts.go
@@ -2,6 +2,7 @@ package testutil
 
 import(
     "fmt"
+    "reflect"
     "testing"
 )
 
@@ -11,7 +12,7 @@ type TestCase struct {
 }
 
 func AssertEq(t *testing.T, have, want interface{}) {
-    if have != want {
+    if !reflect.DeepEqual(have, want) {
         t.Errorf(
             "\nhave: %#v\nwant: %#v",
             have, want,
@@ -19,6 +20,7 @@ func AssertEq(t *testing.T, have, want interface{}) {
     }
 }
 
+// Provides better failure diagnostics than AssertEq().
 func AssertIntSliceEq( t *testing.T, have, want []int ) {
     if len(have) != len(want) {
         t.Errorf( "\nlen(have): %v\nlen(want): %v", len(have), len(want) )
@@ -36,6 +38,7 @@ func AssertIntSliceEq( t *testing.T, have, want []int ) {
     }
 }
 
+// Provides better failure diagnostics than AssertEq().
 func AssertStringSliceEq( t *testing.T, have, want []string ) {
     if len(have) != len(want) {
         t.Errorf( "\nlen(have): %v\nlen(want): %v", len(have), len(want) )
