6065dc5c4ff3d8da70ac34f6c00634f71e92d115
Michael G. Schwern <schwern@pobox.com>
2017-06-02 19:40:55 +0200

    Day 17, switch to a channel of valid combos.
    This will be used for part 2.

--- 2015/day17/day17.go
+++ 2015/day17/day17.go
@@ -4,14 +4,28 @@ type Containers []int
 
 func (self Containers) NumCombos( want int ) int {
     numCombos := 0
-    var i uint
-    for i = 0; i < (1 << uint(len(self))); i++ {
-        if self.combo( i ) == want {
-            numCombos++
-        }
+    for _ = range self.validComboChan(want) {
+        numCombos++
     }
     
     return numCombos
+}
+
+func (self Containers) validComboChan( want int ) chan uint {
+    ch := make(chan uint)
+    
+    go func() {
+        defer close(ch)
+        
+        var i uint
+        for i = 0; i < (1 << uint(len(self))); i++ {
+            if self.combo( i ) == want {
+                ch <- i
+            }
+        }
+    }()
+    
+    return ch
 }
 
 func (self Containers) combo( combo uint ) int {
