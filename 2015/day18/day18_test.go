b3772dd4ca14d449abe11761bf30a5f6e4127fbc
Michael G. Schwern <schwern@pobox.com>
2017-06-03 07:38:16 +0200

    Day 18, Part 1

--- 2015/day18/day18_test.go
+++ 2015/day18/day18_test.go
@@ -3,6 +3,7 @@ package day18_test
 import(
     "testing"
     "github.com/schwern/adventofcode.go/testutil"
+    "github.com/schwern/adventofcode.go/util"
     "github.com/schwern/adventofcode.go/2015/day18"
 )
 
@@ -28,4 +29,15 @@ func TestParseGrid( t *testing.T ) {
     }
     
     testutil.AssertEq( t, day18.ParseGrid(lines), want )
+}
+
+func TestPart1( t *testing.T ) {
+    grid := day18.ParseGridChan( util.LineChannel( InputFile ) )
+    gol := day18.NewGOL( 100, 100, grid )
+    
+    for i := 0; i < 100; i++ {
+        gol.Step()
+    }
+    
+    testutil.AssertEq( t, gol.HowManyLightsDoYouSee(), 821 )
 }
