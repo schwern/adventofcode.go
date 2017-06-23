e0221a229f844c180c47893e49d34ae1bf89b3b8
Michael G. Schwern <schwern@pobox.com>
2017-06-23 12:54:09 -0700

    Day 20, it's faster to do it serially.
    It's even faster then without the mutex.

--- 2015/day20/day20.go
+++ 2015/day20/day20.go
@@ -1,38 +1,16 @@
 package day20
 
-import(
-    "runtime"
-    "sync"
-)
-
 func DeliverPresents( maxHouse, maxDeliveries, numPresents int ) []int {
     presents := make( []int, maxHouse+1 )
 
-    var wg sync.WaitGroup
-    var mapMutex = &sync.Mutex{}
-    deliver := func(start, by int) {
-        defer wg.Done()
-        
-        for elf := start; elf <= maxHouse; elf+=by {
-            numDelivered := 0
+    for elf := 1; elf <= maxHouse; elf++ {
+        numDelivered := 0
             
-            // Putting the lock inside the loop is 10x slower.
-            mapMutex.Lock()
-            for houseNum := elf; houseNum <= maxHouse && numDelivered < maxDeliveries; houseNum += elf {
-                presents[houseNum] += elf * numPresents
-                numDelivered++
-            }
-            mapMutex.Unlock()
+        for houseNum := elf; houseNum <= maxHouse && numDelivered < maxDeliveries; houseNum += elf {
+            presents[houseNum] += elf * numPresents
+            numDelivered++
         }
     }
 
-    startAt := 1
-    numWorkers := runtime.NumCPU() + 1
-    for i := startAt; i < numWorkers+startAt; i++ {
-        wg.Add(1)
-        go deliver(i, numWorkers)
-    }
-    wg.Wait()
-    
     return presents
 }
