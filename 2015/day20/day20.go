3abddde6cb9a193035573e29d36ac51acbd74590
Michael G. Schwern <schwern@pobox.com>
2017-06-23 12:19:58 -0700

    Day 20: Fix a race condition.
    Each goroutine is partitioned by elf, not by houseNum. So they can access
    the same presents[houseNum] concurrently.
    
    Putting the lock tight around the access is a 10x slow down. Around
    the whole loop is 2x. It might kill the benefits of concurrency.

--- 2015/day20/day20.go
+++ 2015/day20/day20.go
@@ -9,15 +9,20 @@ func DeliverPresents( maxHouse, maxDeliveries, numPresents int ) []int {
     presents := make( []int, maxHouse+1 )
 
     var wg sync.WaitGroup
+    var mapMutex = &sync.Mutex{}
     deliver := func(start, by int) {
         defer wg.Done()
         
         for elf := start; elf <= maxHouse; elf+=by {
             numDelivered := 0
+            
+            // Putting the lock inside the loop is 10x slower.
+            mapMutex.Lock()
             for houseNum := elf; houseNum <= maxHouse && numDelivered < maxDeliveries; houseNum += elf {
                 presents[houseNum] += elf * numPresents
                 numDelivered++
             }
+            mapMutex.Unlock()
         }
     }
 
